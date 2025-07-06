8d83aa8a010cb118925397588bf10368977e9c83
Abdiladiif-Abdisamed <cabdiladiifcabdisamed@gmail.com>
2025-07-06 11:17:12 +0300

    added Sign Up and Sign in

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router'
+import { FaUserAlt } from "react-icons/fa";
+import { CiMenuBurger } from "react-icons/ci";
+import { AiOutlineClose } from "react-icons/ai";
+
 
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const [isDropDownOpen, setIsDropDownOpen] = useState(false)
+    const [isLogin, setIsLogin] = useState(false)
+
+    const avatar_url ="https://lh3.googleusercontent.com/a/ACg8ocIyppVHY0V3sEVQEQTh1-FYcxmZtdycobsmEHoetXBsBxGLT2om=s288-c-no"
+
     return (
         <header className='bg-white shadow'>
             <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
@@ -27,21 +37,128 @@ const Header = () => {
                     </div>
                     {/* Right */}
                     <div className='flex items-center  space-x-4'>
-                        {/* profile */}
-                        <div>
-                            <span>Hello, Cabdiladiif</span>
-                        </div>
+                        {/* /* profile */ }
+                        {
+                            isLogin ? (
+                                <>
+                                 <div>
+                                    <span className='text-sm text-gray-700'>Hello, Cabdiladiif</span>
+                                </div>
+                                <div className='relative '>
+                                    <button className='flex justify-center items-center rounded-full h-8 w-8 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 bg-gray-200 cursor-pointer'
+                                    onMouseEnter={()=> setIsDropDownOpen(true)}
+                                    onClick={()=> setIsDropDownOpen(!isDropDownOpen)}>
+                                        {
+                                        avatar_url ? (<img  src={avatar_url}  className='rounded-full h-8 w-8 '/>):
+                                        (<FaUserAlt className='text-orange-500 '/>)
+                                    }
+                                    </button>
 
-                        {/* SignUp and SIgnin */}
+                                    {/* DropDown Menu */}
 
-                        <div className='flex items-center  space-x-4'>
+                                    {
+                                        isDropDownOpen && (
+                                            <div className='absolute right-0 w-48 bg-white mt-1 rounded-md shadow-lg z-50 '
+                                            onMouseLeave={()=> setIsDropDownOpen(false)}>
+                                                <div className='absolute h-3 w-full top-[12px]'></div>
+                                                <Link className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100'>Your Profile</Link>
+
+                                                 <Link className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100'>Manage Articles</Link>
+
+                                                  <Link className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100'>Sign Out </Link>
+                                                 
+                                            </div>
+                                        )
+                                    }
+                                    
+                                </div>
+                                </>
+                               
+                            ) :
+                            (
+                            
+                             <div className='flex items-center  space-x-4'>
                             <Link to='/signin' className='inline-flex items-center px-4 py-2  bg-orange-700 text-sm font-medium text-white rounded-md hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500'>SignIn</Link>
 
                             <Link to='/signup' className='inline-flex items-center px-4 py-2  border border-orange-700 text-sm font-medium text-orange-600 rounded-md hover:bg-orange-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500'>SignUp</Link>
                         </div>
+                                
+                            )
+                        }
+
+
+                        {/* SignUp and SIgnin */}
+
+                      
                     </div>
+                {/* Harburger  */}
+                <div className='-mr-2 flex items-center sm:hidden '>
+                        <button className='inline-flex items-center justify-center p-2 rounded-md text-gray-400'
+                        onClick={()=> setIsMenuOpen(!isMenuOpen)}>
+                            {
+                                isMenuOpen ? <AiOutlineClose className='block h-6 w-6 cursor-pointer'/> : <CiMenuBurger className='block h-6 w-6 cursor-pointer'/>
+
+
+                            }
+                        </button>
                 </div>
+                </div>
+
+                
             </div>
+
+             {isMenuOpen && (<div className='sm:hidden py-4'>
+                <div className='pt-2 pb-3 space-y-1'>
+                    <Link to="/" className="block pl-3 pr-4 py-2 border-l-4 border-orange-500 text-base font-medium text-orange-700 bg-orange-50">
+                        Home
+                    </Link>
+                    <Link to="/articles" className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800">
+                        Articles
+                    </Link>
+                </div>
+
+
+                {/* if is logged in */}
+
+                {isLogin && (
+                    <>
+                        <Link to="/editor" className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800">
+                            Write
+                        </Link>
+                        <Link to="/manage-articles" className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800">
+                            My Articles
+                        </Link>
+                        <Link to="/profile" className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800">
+                            Profile
+                        </Link>
+                        <button
+                            onClick={() => {
+                                alert("hhhh")
+                            }}
+
+                            className="block w-full text-left pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
+                        >
+                            Sign Out
+                        </button>
+                    </>
+                )}
+
+
+                {/* if is not logged in */}
+
+                {!isLogin && (
+                    <>
+                        <Link to="/signin" className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800">
+                            Sign In
+                        </Link>
+                        <Link to="/signup" className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800">
+                            Sign Up
+                        </Link>
+                    </>
+                )}
+
+            </div>
+            )}
         </header>
     )
 }
